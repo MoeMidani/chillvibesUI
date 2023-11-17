1e39914acff96f20debaa68724ab9fde6ff8bac7
Jennifer_Simond <jenniasimond@gmail.com>
2023-11-17 03:58:33 -0500

    fin

--- SwiftUIStoryTemplate/QuizView.swift
+++ SwiftUIStoryTemplate/QuizView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct QuizView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("The Quiz didn't make it! 🤷🏽‍♀️")
+            
     }
 }
 

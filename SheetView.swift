2491c4c28c011eaf0f17f8f247b97aa0dc9d0766
Jordan Allen <jordanallen93@gmail.com>
2023-11-17 08:25:22 -0500

    sbfk

---
+++ SheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetView.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Jordan Allen on 11/16/23.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetView()
+}
--- SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -1,24 +1,67 @@
 import SwiftUI
     
-    struct Chapter4View: View {
-        var characterPronouns = ["they", "their", "theirs", "themself", "kid","them","they're"]
-        
-        @State private var buttonTapped = true
-        
-        var body: some View {
-            VStack {
-                Button("Chapter 4") {
-                    print ("Enter Chapter 4")
-                    self.buttonTapped =
-                    true
-                }
-                .padding ()
-                if buttonTapped{
-                    //Text ("About now \(characterName) is hungry. \(characterPronouns[6]) already eaten the food  \(characterPronouns[6]) packed for the trip but it’s a long journey to the finish line. \(characterPronouns[0].capitalized) can tell \(characterPronouns[6]) almost there! But \(characterPronouns[0]) needs that extra push to continue. Around this area is where \(characterPronouns[1]) and grandpa George would stop to rest and eat a snack. There is a ton of beautiful berries and leaves grandpa would pull off and eat. \(characterPronouns[1].capitalized) favorite was the blackberry bush her grandpa and grandma planted. But which way is it?")
-                        //.padding()
+struct Chapter4View: View {
+    
+    
+    //var characterPronouns = ["they", "their", "theirs", "themself", "kid","them","they're"]
+    //switch characterGender {
+    
+    //var characterPronouns = ["they", "their", "theirs", "themself", "kid","them","they're"]
+    //var characterGender
+    // var characterGender = gender.lowercased()
+    //var characterPronouns [0 = subject pronoun: he, she they,
+    // 1 = object pronoun: his, her, their,
+    //2 = is a possessive pronoun: his, hers, theirs,
+    //3 = added for himself, hersolf, themself,
+    //4 = added for direct gender reference: boy, girl, kid,
+    //5 = added for him, her, them,
+    //6 = added for hes, shes, theyre]
+    
+    //case "b":
+    //characterPronouns = ["he", "his", "his", "himself","boy","him","he's"]
+    //case "g":
+    // characterPronouns = ["she", "her", "hers", "herself","girl","her","she's"]
+    // case "n":
+    //  characterPronouns = ["they", "their", "theirs", "themself", "kid","them","they're"]
+    
+    
+    @State private var buttonTapped = false
+    @State private var buttonTapped2 = false
+    
+    var body: some View {
+        VStack {
+            Button("Chapter 4") {
+                print ("Enter Chapter 4")
+                self.buttonTapped =
+                true
+            }
+            .padding ()
+            if buttonTapped == true {
+                VStack {
+                    Image ("forestPath3")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    Text ("About now Jordan is hungry. She’s already eaten the food she packed for the trip but it’s a long journey to the finish line. She can tell she’s almost there! But she needs that extra push to continue. Around this area is where her and grandpa George would stop to rest and eat a snack. There is a ton of beautiful berries and leaves grandpa would pull off and eat. Her favorite was the blackberry bush her grandpa and grandma planted. But which way is it?")
+                    
+                  //  struct SheetView: View {
+                    @State var isPresented = false
+                        
+                        Button ( "Enter Left or Right") {
+                            print ("Left or Right")
+                        
+                        
+                            self.buttonTapped2 == true
+                            ($buttonTapped2)
+                            }
+                            }
+                            }
+                        }
+                    
                 }
             }
-        }
-        
+    
+#Preview {
+        Chapter4View()
     }
 
